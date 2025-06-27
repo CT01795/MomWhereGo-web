b035afab4fe4607e0faf61ed906bf2c841c772ac
CT01795 <CT01795@users.noreply.github.com>
2025-06-27 07:10:06 +0000

    deploy: 8ed7ac68b17bed9d096aba6247d4c721579506f2

--- android/app/src/main/kotlin/com/example/mom_where_go/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mom_where_go
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/export_util_stub.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:mom_where_go/models/event.dart';
-
-Future<void> exportEventsToExcel(BuildContext context, List<Event> events) async {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(content: Text('⚠️ 此平台尚未支援匯出 Excel')),
-  );
-}
